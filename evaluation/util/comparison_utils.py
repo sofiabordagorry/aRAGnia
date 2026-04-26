e73759000ba9fc94c7c27907637526024ab3dc5e
matimarin456 <151960359+matimarin456@users.noreply.github.com>
2026-04-26 16:28:38 -0300

    Fix indentation in comparison_utils.py

--- evaluation/util/comparison_utils.py
+++ evaluation/util/comparison_utils.py
@@ -61,7 +61,7 @@ def entity_key(entity: dict, use_id: bool = False) -> Tuple:
                 value.get("base_name", ""),
                 value.get("sub_id", ""),
                 value.get("type", ""),
-                value.get("is_group", "")
+                value.get("is_group", ""),
                 value.get("year_publisher","")
             )
 
